1883a6777cd37f4650c1d3d476ee775c49fb4afd
Jose A. Tolentino <61167951+jatolentino@users.noreply.github.com>
2022-01-26 12:19:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function myscript() {
 	
 	
 	for (var i=0; i < firstmatch.length;i++){
-		newtext = newtext.replace(firstmatch[i], prefix.concat(matchesnonrepeat[i],"} "));
+		newtext = newtext.replaceAll(firstmatch[i], prefix.concat(matchesnonrepeat[i],"} "));
 		myText = newtext;
 		newtext = newtext.replace(secondmatch[i], prefix.concat(matchesnonrepeat[i],"}, "));
 		myText = newtext;
